3bbf986323ae4a1f3f7da7fea75ab298fd543faf
Shanzson <shanzson@Shanzsons-Laptop.local>
2024-06-15 07:24:30 +0530

    Adding tests

--- src/BlastDapp.sol
+++ src/BlastDapp.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.24;
 
 import "openzeppelin-contracts/contracts/access/extensions/AccessControlDefaultAdminRules.sol";
 import "./interfaces/IBlastPoints.sol";
-contract AIA_Dapp is AccessControlDefaultAdminRules{
+contract BlastDapp is AccessControlDefaultAdminRules{
     address public BlastPointsAddress;
   constructor(address _BlastPointsAddress, address _pointsOperator,  uint48 _initialDelay, address _initialDefaultAdmin) AccessControlDefaultAdminRules( _initialDelay, _initialDefaultAdmin){
     BlastPointsAddress = _BlastPointsAddress;
